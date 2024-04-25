df7fa124b02e81cf7f5139cc62649ac90053ee84
Atharva <atharvagurao261@gmail.com>
2024-04-26 00:54:10 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,193 @@
+var product = [
+    {
+        "id": 1,
+        "title": "Maruti Suzuki Alto",
+        "price": "90000",
+        "description": "N/A",
+        "brand": "Maruti",
+        "image": "./alto.webp",
+        "year": "2018",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 2,
+        "title": "Toyota Etios",
+        "price": "80000",
+        "description": "N/A",
+        "brand": "Toyota",
+        "image": "./etios.jpeg",
+        "year": "2018",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 3,
+        "title": "Toyota Fortuner",
+        "price": "550000",
+        "description": "N/A",
+        "brand": "Toyota",
+        "image": "./image.png",
+        "year": "2016",
+        "fuelType": "Diesel"
+    },
+    {
+        "id": 4,
+        "title": "Honda Amaze",
+        "price": "180000",
+        "description": "N/A",
+        "brand": "Honda",
+        "image": "./amaze.jpg",
+        "year": "2015",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 5,
+        "title": "Honda City",
+        "price": "160000",
+        "description": "N/A",
+        "brand": "Honda",
+        "image": "./hondacity.jpeg.webp",
+        "year": "2019",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 6,
+        "title": "Hyundai Eon",
+        "price": "130000",
+        "description": "N/A",
+        "brand": "Hyundai",
+        "image": "./hyundai-eon.webp",
+        "year": "2016",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 7,
+        "title": "Hyundai i20 Active",
+        "price": "170000",
+        "description": "N/A",
+        "brand": "Hyundai",
+        "image": "./Hyundai-i20-Active-.webp",
+        "year": "2017",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 8,
+        "title": "Honda Civic",
+        "price": "220000",
+        "description": "N/A",
+        "brand": "Honda",
+        "image": "./civic.jpeg.webp",
+        "year": "2019",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 9,
+        "title": "Tata Tiago",
+        "price": "80000",
+        "description": "N/A",
+        "brand": "Tata",
+        "image": "./tiago.jpg.webp",
+        "year": "2017",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 10,
+        "title": "Ford Aspire",
+        "price": "140000",
+        "description": "N/A",
+        "brand": "Ford",
+        "image": "./ford_Aspire_.jpg.webp",
+        "year": "2016",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 11,
+        "title": "Chevrolet Spark",
+        "price": "110000",
+        "description": "N/A",
+        "brand": "Chevrolet",
+        "image": "./saprk.webp",
+        "year": "2016",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 12,
+        "title": "Fiat Punto",
+        "price": "120000",
+        "description": "N/A",
+        "brand": "Fiat",
+        "image": "./punto.jpg.webp",
+        "year": "2016",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 13,
+        "title": "Skoda Rapid",
+        "price": "170000",
+        "description": "N/A",
+        "brand": "Skoda",
+        "image": "./Skoda-Rapid.jpg-2.webp",
+        "year": "2018",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 14,
+        "title": "Renault Duster",
+        "price": "160000",
+        "description": "N/A",
+        "brand": "Renault",
+        "image": "./duster.jpg.webp",
+        "year": "2016",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 15,
+        "title": "Volkswagen Polo",
+        "price": "130000",
+        "description": "N/A",
+        "brand": "Volkswagen",
+        "image": "./polo.jpg.webp",
+        "year": "2018",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 16,
+        "title": "Hyundai Verna",
+        "price": "140000",
+        "description": "N/A",
+        "brand": "Hyundai",
+        "image": "./verna.jpg.webp",
+        "year": "2016",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 17,
+        "title": "Toyota Innova",
+        "price": "250000",
+        "description": "N/A",
+        "brand": "Toyota",
+        "image": "./Inova.jpeg.webp",
+        "year": "2017",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 18,
+        "title": "Mahindra XUV500",
+        "price": "350000",
+        "description": "N/A",
+        "brand": "Mahindra",
+        "image": "./xuv500.webp",
+        "year": "2016",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": 19,
+        "title": "Maruti Swift",
+        "price": "80000",
+        "description": "N/A",
+        "brand": "Maruti",
+        "image": "./swift-.jpeg.webp",
+        "year": "2018",
+        "fuelType": "Petrol"
+    },
+   
+]
